d07a1de336a15e8c286a5b1dd45ce3170f5a09e0
Harris Sultan <harris_sultan@outlook.com>
2022-01-13 10:46:13 -0500

    commit tree a45904e464ae7e8160f982d10a9ba36f482cb7d0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const a = record.find(record => record.result === "W")
+    return !!a? a.year : undefined
+    // if (!a!==a){
+    //     return a.year
+    // }else{
+    // return undefined
+    // }
+}
